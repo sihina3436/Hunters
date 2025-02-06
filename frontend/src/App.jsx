c65be4eac9b9d0a8ea2384ea4b2a80659fd0ac6f
sihina3436 <nimnadasihina@gmail.com>
2025-02-07 00:53:27 +0530

    Initial Commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Button } from "flowbite-react";
+
+const App = () => {
+  return (
+    <div className="flex h-screen items-center justify-center bg-gray-100">
+    <h1 className="text-3xl font-bold text-blue-600">Hello, Tailwind with Vite!</h1>
+    <Button>Click me</Button>
+  </div>
+  )
+}
+
+export default App
