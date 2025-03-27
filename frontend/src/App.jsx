429a523a665ced2b5981ce7785c51b5a143f7259
sihina3436 <nimnadasihina@gmail.com>
2025-03-27 23:52:27 +0530

    Remove unnecessary code

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,27 +1,10 @@
 import React from 'react'
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
-import Header from './components/Header';
-import About from './pages/About';
-import Signin from './pages/Signin';
-import Signup from './pages/Signup';
-import Home from './pages/Home';
-import Footer from './components/Footer';
 
 
 const App = () => {
   return (
-    <div>
-      <BrowserRouter>
-      <Header/>
-      <Routes>
-        <Route path='/' element={<Home/>}></Route>
-        <Route path='/about' element={<About/>}></Route>
-        <Route path='/signin' element={<Signin/>}></Route>
-        <Route path='/signup' element={<Signup/>}></Route>
-      </Routes>
-      <Footer/>
-    </BrowserRouter>
-    </div>
+   <>
+   </>
     
   )
 }
