f071d54f48fb904e845552527c1bafc85bc704bc
sihina3436 <nimnadasihina@gmail.com>
2025-03-27 23:42:01 +0530

    Create responsive navigation bar

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -4,7 +4,22 @@ export default {
   content: [    "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",flowbite.content(),],
   theme: {
-    extend: {},
+    extend: { maxWidth: {
+      'screen-2xl': '1400px', 
+      'custom-1200': '1200px', 
+      'custom-900': '900px', 
+    },
+    colors: {
+      'primary': '#e53888',
+      'primary-dark': "#AC1754",
+      'primary-light': '#FFEDFA',
+      'text-dark': '#0f172a',
+      'text-light': '#64748b',
+      'extra-light': '#f8fafc'
+    },
+    fontFamily: {
+      custom: ['Roboto', 'sans-serif'],  // Replace 'Roboto' with your font name
+    },},
   },
   plugins: [  flowbite.plugin(),],
 }
