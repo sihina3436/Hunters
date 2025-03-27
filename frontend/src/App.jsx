79b7da7c82c6c7a04bdf8c5bde46cc9971092218
sihina3436 <nimnadasihina@gmail.com>
2025-03-27 23:42:01 +0530

    Create responsive navigation bar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,29 +1,15 @@
 import React from 'react'
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
-import Header from './components/Header';
-import About from './pages/About';
-import Signin from './pages/Signin';
-import Signup from './pages/Signup';
-import Home from './pages/Home';
-import Footer from './components/Footer';
-
+import Navbar from './components/Navbar'
+import { Outlet } from 'react-router-dom'
 
 const App = () => {
   return (
-    <div>
-      <BrowserRouter>
-      <Header/>
-      <Routes>
-        <Route path='/' element={<Home/>}></Route>
-        <Route path='/about' element={<About/>}></Route>
-        <Route path='/signin' element={<Signin/>}></Route>
-        <Route path='/signup' element={<Signup/>}></Route>
-      </Routes>
-      <Footer/>
-    </BrowserRouter>
-    </div>
-    
+    <>
+      <Navbar/>
+      <Outlet/>
+    </>
   )
 }
 
 export default App
+
--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
-const Header = () => {
+const Home = () => {
   return (
     <div>
-      This is Header
+      
     </div>
   )
 }
 
-export default Header
+export default Home
