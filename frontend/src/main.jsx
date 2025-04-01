9f32c41f66043915ca5ce18fa4515c2a26a473d2
sihina3436 <nimnadasihina@gmail.com>
2025-04-01 18:09:50 +0530

    Install Redux and set up Redux for state management

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,10 +4,11 @@ import { RouterProvider } from "react-router-dom"
 import Router from './router/Router'
 import './index.css'
 import "remixicon/fonts/remixicon.css";
-
+import { store } from "./redux/store.js";
+import { Provider } from "react-redux";
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  <Provider store={store}>
       <RouterProvider router={Router} />
-  </StrictMode>,
+  </Provider>
 )
