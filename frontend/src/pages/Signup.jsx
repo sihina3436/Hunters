3db11f030fd65ca66978db4fc3b4365a3489de4b
Kosala Gunarathna <140527096+KosalaGunarathna@users.noreply.github.com>
2025-04-02 15:23:39 +0530

    make login and sigup with redux

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,75 +1,67 @@
-import React,{useState} from 'react'
-import { Alert, Button, Label, Spinner, TextInput } from 'flowbite-react';
-import { VscEye,VscEyeClosed } from "react-icons/vsc";
+import React, { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom';
+import { useRegisterUserMutation } from '../redux/features/auth/authApi.js'
 
+const Register = () => {
+    const [message, setMessage] = useState('');
+    const [username, setUsername] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
 
-const Signup = () => {
-  const [showPassword,setOpen]= useState(false)
-  const toggle=()=>{
-    setOpen(!showPassword)
-  }
+    const [registerUser, { isLoading }] = useRegisterUserMutation();
+    const navigate = useNavigate()
 
-  return (
-    
-      <div className="flex justify-center items-center h-screen"> 
-        <div className=" px-5 py-15  bg-zinc-200 w-full mx-3 md:w-1/2 rounded-3xl">
-          <h1 className='text-center font-bold text-3xl p-2 py-6'>Sign Up</h1>
-            <form className='space-y-6 action="#" method="POST" '>
-          
-              <div >
-                <Label value='Enter Username'></Label>
-                <TextInput type="text" placeholder='Username' id='username' className='mt-2'></TextInput>
-                
-              </div>
+    const handleRegister = async (e) => {
+        e.preventDefault();
+        const data = {
+            username,
+            email,
+            password
+        }
+        try {
+            await registerUser(data).unwrap();
+            alert("Registration successful!")
+            navigate('/login')
+        } catch (error) {
+            setMessage("Registration failed")
+        }
 
-              <div>
-                <Label value='Enter Email Address'></Label>
-                <TextInput type="email" placeholder='email' id='email' className='  mt-2'></TextInput>
-              </div>
+    }
+    return (
+        <section className='h-screen flex items-center justify-center'>
+            <div className='max-w-sm border shadow bg-white mx-auto p-8'>
+                <h2 className='text-2xl font-semibold pt-5'>Please Register</h2>
+                <form onSubmit={handleRegister} className='space-y-5 max-w-sm mx-auto pt-8'>
+                    <input type="text" name="username" id="username"
+                        onChange={(e) => setUsername(e.target.value)}
+                        placeholder='username' required
+                        className='w-full bg-gray-100 focus:outline-none px-5 py-3'
+                    />
+                    <input type="email" name="email" id="email"
+                        onChange={(e) => setEmail(e.target.value)}
+                        placeholder='Email Address' required
+                        className='w-full bg-gray-100 focus:outline-none px-5 py-3'
+                    />
+                    <input type="password" name="password" id="password"
+                        onChange={(e) => setPassword(e.target.value)}
+                        placeholder='Password' required
+                        className='w-full bg-gray-100 focus:outline-none px-5 py-3'
+                    />
+                    {
+                        message && <p className='text-red-500'>{message}</p>
+                    }
 
+                    <button type='submit'
+                        className='w-full mt-5 bg-primary text-white hover:bg-indigo-500 font-medium py-3 rounded-md'
+                    >Register</button>
+                </form>
 
-              <div className="relative">
-                <Label value='Enter Password'></Label>
-                  <TextInput type={(showPassword===false) ? 'password' : password} placeholder='Enter 8 characters or more' id='password' className=' mt-2'></TextInput>
-                  <div className='text-2xl absolute top-10 right-5'>
-                  {
-                    (showPassword===false)?<VscEye onClick={toggle} />:
-                    <VscEyeClosed onClick={toggle}/>
-                  }
-                  </div>
-              </div>
-
-              <div >
-                <Label value='Re Enter Password'></Label>
-                <TextInput type="password" placeholder='Re Enter Password' id='password2' className='mt-2'></TextInput>
-              </div>
-
-              <div>
-                <Label className=' text-red-600'>Invalid password </Label>
-                
-                
-              </div>
-              <button type="submit" 
-              class="items-center min-w-full text-white bg-fuchsia-300 outline-5 
-               font-bold rounded-full  px-5 py-2.5 text-center ">Sign Up</button>
-
-            </form>
-              <div className="other-obtion my-3">
-                <p className='text-center '>Or Continue with other ways</p>
-                <div className="icons flex justify-center gap-10 py-5">
-
-                  <div className="1 bg-black w-20 h-20" ></div>
-                  <div className="2  bg-black w-20 h-20"></div>
-                  <div className="3  bg-black w-20 h-20"></div>
-                </div>
-              </div>
-
-
-          </div>
-      </div>
-    
-  
-  )
+                <p className='my-5 italic text-sm text-center'>Have an account? Please
+                    <Link to="/login" className='text-red-700 px-1 underline'>Login</Link>.
+                </p>
+            </div>
+        </section>
+    )
 }
 
-export default Signup
+export default Register
