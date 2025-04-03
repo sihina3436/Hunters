3582ed0b1526d803a4d6009f2da9a39ef3b84d2f
SiharaH <siharaedirisinghe03@gmail.com>
2025-04-03 15:17:23 +0530

    Create Footer Section

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import Features from './Features'
 import MainBanner from './MainBanner'
+import Promotions from './Promotion'
+import FeaturedProducts from '../shop/FeaturedProducts'
+import SpecialEdition from './SpecialEdition'
 
 const Home = () => {
   return (
     <div>
       <MainBanner/>
+      <Promotions/>
+      <FeaturedProducts/>
+      <SpecialEdition/>
       <Features/>
     </div>
   )
