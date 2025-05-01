c2b89703382742fcfff2c67e117b344055023572
Theekshana1999 <theekshana.amamadu@gmail.com>
2025-05-02 01:23:23 +0530

    create private Routes

--- frontend/src/router/Router.jsx
+++ frontend/src/router/Router.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 import { createBrowserRouter } from 'react-router-dom'
 import App from '../App'
 import Home from '../pages/home/Home';
+import PaymentSuccess from '../components/PaymentSuccess';
+import DashBoardLayout from '../pages/dashboard/DashBoardLayout.jsx';
+import UserDMain from '../pages/dashboard/user/dashboard/userDMain.jsx';
+import AdminDMain from '../pages/dashboard/admin/dashboard/AdminDMain.jsx';
 
 
 const Router = createBrowserRouter([
