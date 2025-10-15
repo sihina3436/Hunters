f523852eafda2957354323d0396d34ec33c87644
H.K.Sihina Nimnada <nimnadasihina@gmail.com>
2025-10-15 20:47:15 +0530

    Update CORS origin to production URL

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(
   cors({
-    origin: 'http://localhost:5173', 
+    origin: 'https://www.zerozclothes.com/', 
     credentials: true,
   })
 );
